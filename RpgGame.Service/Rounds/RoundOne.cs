75394d025d77b5c67ac21a744e4ea9c29d07c7a7
chrs-m <mister.ch@gmail.com>
2022-03-18 15:48:31 +0100

    Add start of round 2

--- RpgGame.Service/Rounds/RoundOne.cs
+++ RpgGame.Service/Rounds/RoundOne.cs
@@ -16,17 +16,17 @@ public class RoundOne : Round
         var ms = 1000;
         
         IO.Out($"Välkommen {Player.Name}!");
-        Thread.Sleep(ms);
+        //Thread.Sleep(ms);
         IO.Out("Du vaknar upp lite halvtrasig i slottskogen efter en utekväll..");
-        Thread.Sleep(ms);
+        //Thread.Sleep(ms);
         IO.Out($"Bredvid dig hittar du {Items.Weapons[0].Name}({Items.Weapons[0].Damage}dmg) och en {Items.Food[0].Name}.");
-        Thread.Sleep(ms);
+        //Thread.Sleep(ms);
         IO.Out($"{Items.Food[0].Name} ger dig lite extra energi ({Items.Food[0].Modifier}).");
 
         Player.Weapon = Items.Weapons[0];
         Player.Consume(Items.Food[0]);
         
-        Thread.Sleep(ms);
+        //Thread.Sleep(ms);
         IO.Out($"Du har nu {Player.Hp}hp och ditt vapen är {Player.Weapon.Name} ({Player.Weapon.Damage}dmg).");
 
         Console.WriteLine();
--- RpgGame.Service/Rounds/RoundTwo.cs
+++ RpgGame.Service/Rounds/RoundTwo.cs
@@ -11,6 +11,30 @@ public class RoundTwo : Round
     {
         Player = player;
     }
-    
-    public override void StartRound() { }
+
+    public override void StartRound()
+    {
+        var random = new Random();
+        int index = random.Next(Items.Food.Count);
+        var randomFood = Items.Food[index];
+        
+        IO.Out($"När du kommer till säldammen så ser du {randomFood.Name} på marken.");
+        IO.Out("Vill du plocka upp och förtära det du hittade?");
+        IO.Out("1 för ja, 2 för nej");
+        var inputAnswer = IO.IntIn("Gör ditt val: ");
+        
+        if (inputAnswer == 1)
+        {
+            Player.Consume(randomFood);
+            IO.Out($"Du förtärde {randomFood.Name}, ditt hp är nu {Player.Hp}");
+        }
+        //random food/object round
+        //choose to take food/object
+
+        //message
+        //choice
+        //events
+        //outcome
+        //move on
+    }
 }
